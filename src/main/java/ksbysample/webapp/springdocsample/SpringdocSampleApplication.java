3490589034d700bea76c38a31611319ca1b5c5cd
ksby <kusa@db3.so-net.ne.jp>
2021-03-24 13:37:37 +0000

    @SpringBootApplication 付与クラスに @OpenAPIDefinition アノテーションを付与して API の情報を追加しました

--- src/main/java/ksbysample/webapp/springdocsample/SpringdocSampleApplication.java
+++ src/main/java/ksbysample/webapp/springdocsample/SpringdocSampleApplication.java
@@ -1,5 +1,7 @@
 package ksbysample.webapp.springdocsample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * springdoc-openapi サンプル Web アプリ用 Application クラス
  */
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ksbysample-springdoc-openapi-sample",   // (1)
+        description = "Spring Boot + springdoc-openapi のサンプルアプリ",           // (2)
+        version = "v1"))    // (3)
 public class SpringdocSampleApplication {
 
     /**
